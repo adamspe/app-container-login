329e0fbb6480ebb82242991bf0f5da91000d3fdc
Paul Adams <adamspe@gmail.com>
2017-01-09 23:55:41 -0600

    follow the app module convention

--- index.js
+++ index.js
@@ -2,49 +2,51 @@
 // the including module must be based on app-container and
 // include what is necessary, this module is broken out like this
 // so it can be used in test environments where necessary
-module.exports = function(container,test) {
-    var debug = require('debug')('login'),
-        AppContainer = require('app-container'),
-        User = AppContainer.userModel(),
-        app = container.app(),
-        passport = require('passport'),
-        LocalStrategy = require('passport-local').Strategy;
+module.exports = {
+    init: function(container,test) {
+        var debug = require('debug')('login'),
+            AppContainer = require('app-container'),
+            User = AppContainer.User,
+            app = container.app(),
+            passport = require('passport'),
+            LocalStrategy = require('passport-local').Strategy;
 
-    passport.use(new LocalStrategy(
-        function(username,password,done) {
-            debug('login(%s,%s)',username,(!!password ? '****' : ''));
-            User.findOne({
-                email: username
-            },function(err,user){
-                if(err) {
-                    return done(err);
-                }
-                debug('found user %s',user);
-                if(!user) {
-                    return done(null,false,{ message: 'Unknown username or password.'});
-                }
-                user.validatePassword(password,function(err,res){
-                    if(!res) {
-                        return done(null, false, { message: 'Unknown username or password.' });
+        passport.use(new LocalStrategy(
+            function(username,password,done) {
+                debug('login(%s,%s)',username,(!!password ? '****' : ''));
+                User.findOne({
+                    email: username
+                },function(err,user){
+                    if(err) {
+                        return done(err);
                     }
-                    return done(null,user);
+                    debug('found user %s',user);
+                    if(!user) {
+                        return done(null,false,{ message: 'Unknown username or password.'});
+                    }
+                    user.validatePassword(password,function(err,res){
+                        if(!res) {
+                            return done(null, false, { message: 'Unknown username or password.' });
+                        }
+                        return done(null,user);
+                    });
                 });
+            }
+        ));
+
+        app.post('/login',
+            passport.authenticate('local', {
+                successRedirect: '/',
+                failureRedirect: '/login',
+                failureFlash: true
+           })
+        );
+
+        if(test) {
+            app.get('/logout', function(req, res){
+              req.logout();
+              res.redirect('/');
             });
         }
-    ));
-
-    app.post('/login',
-        passport.authenticate('local', {
-            successRedirect: '/',
-            failureRedirect: '/login',
-            failureFlash: true
-       })
-    );
-
-    if(test) {
-        app.get('/logout', function(req, res){
-          req.logout();
-          res.redirect('/');
-        });
     }
 };
